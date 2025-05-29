ca0909b1fd30ec54e2dc9c9a507abddfffb20769
valeria.benitez889@pascualbravo.edu.co <valeria.benitez889@pascaulbravo.edu.co>
2025-05-29 05:05:51 -0500

    dd GymConnection: Implementación de la clase singleton para la conexión a la base de datos

---
+++ AccesoDatos/GymConnection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace GymApp.AccesoDatos
+{
+    public class GymConnection
+    {
+        private static GymConnection instance;
+        public SqlConnection Connection { get; private set; }
+
+        private GymConnection()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["GymDB"].ConnectionString;
+            Connection = new SqlConnection(connectionString);
+        }
+
+        public static GymConnection GetInstance()
+        {
+            if (instance == null)
+                instance = new GymConnection();
+            return instance;
+        }
+    }
+}
