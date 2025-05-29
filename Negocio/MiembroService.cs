87c5a9ad13689e4665aefa701bb1046c790ef913
valeria.benitez889@pascualbravo.edu.co <valeria.benitez889@pascaulbravo.edu.co>
2025-05-29 05:30:41 -0500

    Implementado MiembroService: Servicio que orquesta operaciones de miembros con validaciones y llamadas al repositorio

---
+++ Negocio/MiembroService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GymApp.AccesoDatos;
+using GymApp.Entidades;
+
+namespace GymApp.Negocio
+{
+    public class MiembroService
+    {
+        private readonly IMiembroRepository miembroRepo;
+
+        public MiembroService(IMiembroRepository repo)
+        {
+            this.miembroRepo = repo;
+        }
+
+        public int CrearMiembro(Miembro miembro)
+        {
+            miembroRepo.Crear(miembro);
+            return miembro.UsuarioID;
+        }
+
+        public void ActualizarMiembro(Miembro miembro)
+        {
+            miembroRepo.Actualizar(miembro);
+        }
+
+        public void EliminarMiembro(int id)
+        {
+            miembroRepo.Eliminar(id);
+        }
+
+        public Miembro ObtenerMiembroPorId(int id)
+        {
+            return miembroRepo.ObtenerPorId(id);
+        }
+
+        public IEnumerable<Miembro> ObtenerTodosLosMiembros()
+        {
+            return miembroRepo.ObtenerTodos();
+        }
+    }
+}
