e93ac78e2a0b85244742ac4a199be29bd8e7c8b8
valeria.benitez889@pascualbravo.edu.co <valeria.benitez889@pascaulbravo.edu.co>
2025-05-29 14:36:05 -0500

    Implementación completa de ReservaForm: formulario para registrar reservas, integración con ReservaService y carga dinámica de clases

--- Presentacion/ReservaForm.Designer.cs
+++ Presentacion/ReservaForm.Designer.cs
@@ -32,6 +32,9 @@
             this.btnReservar = new System.Windows.Forms.Button();
             this.dtpFechaReserva = new System.Windows.Forms.DateTimePicker();
             this.cmbClases = new System.Windows.Forms.ComboBox();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblClases = new System.Windows.Forms.Label();
+            this.lblFechaReserva = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblMensaje
@@ -39,9 +42,9 @@
             this.lblMensaje.AutoSize = true;
             this.lblMensaje.Location = new System.Drawing.Point(300, 343);
             this.lblMensaje.Name = "lblMensaje";
-            this.lblMensaje.Size = new System.Drawing.Size(35, 13);
+            this.lblMensaje.Size = new System.Drawing.Size(16, 13);
             this.lblMensaje.TabIndex = 7;
-            this.lblMensaje.Text = "label1";
+            this.lblMensaje.Text = "...";
             // 
             // btnReservar
             // 
@@ -49,12 +52,12 @@
             this.btnReservar.Name = "btnReservar";
             this.btnReservar.Size = new System.Drawing.Size(75, 23);
             this.btnReservar.TabIndex = 6;
-            this.btnReservar.Text = "button1";
+            this.btnReservar.Text = "Reservar";
             this.btnReservar.UseVisualStyleBackColor = true;
             // 
             // dtpFechaReserva
             // 
-            this.dtpFechaReserva.Location = new System.Drawing.Point(300, 174);
+            this.dtpFechaReserva.Location = new System.Drawing.Point(312, 174);
             this.dtpFechaReserva.Name = "dtpFechaReserva";
             this.dtpFechaReserva.Size = new System.Drawing.Size(200, 20);
             this.dtpFechaReserva.TabIndex = 5;
@@ -67,11 +70,41 @@
             this.cmbClases.Size = new System.Drawing.Size(121, 21);
             this.cmbClases.TabIndex = 4;
             // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(300, 28);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(83, 13);
+            this.lblTitulo.TabIndex = 8;
+            this.lblTitulo.Text = "Realizar reserva";
+            // 
+            // lblClases
+            // 
+            this.lblClases.AutoSize = true;
+            this.lblClases.Location = new System.Drawing.Point(167, 98);
+            this.lblClases.Name = "lblClases";
+            this.lblClases.Size = new System.Drawing.Size(99, 13);
+            this.lblClases.TabIndex = 9;
+            this.lblClases.Text = "Seleccione la clase";
+            // 
+            // lblFechaReserva
+            // 
+            this.lblFechaReserva.AutoSize = true;
+            this.lblFechaReserva.Location = new System.Drawing.Point(167, 174);
+            this.lblFechaReserva.Name = "lblFechaReserva";
+            this.lblFechaReserva.Size = new System.Drawing.Size(129, 13);
+            this.lblFechaReserva.TabIndex = 10;
+            this.lblFechaReserva.Text = "Fecha y Hora de Reserva";
+            // 
             // ReservaForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblFechaReserva);
+            this.Controls.Add(this.lblClases);
+            this.Controls.Add(this.lblTitulo);
             this.Controls.Add(this.lblMensaje);
             this.Controls.Add(this.btnReservar);
             this.Controls.Add(this.dtpFechaReserva);
@@ -90,5 +123,8 @@
         private System.Windows.Forms.Button btnReservar;
         private System.Windows.Forms.DateTimePicker dtpFechaReserva;
         private System.Windows.Forms.ComboBox cmbClases;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblClases;
+        private System.Windows.Forms.Label lblFechaReserva;
     }
 }
--- Presentacion/ReservaForm.cs
+++ Presentacion/ReservaForm.cs
@@ -10,27 +10,85 @@ using System.Windows.Forms;
 using GymApp.Entidades;
 using GymApp.Negocio;
 using GymApp.AccesoDatos;
-
 namespace GymApp.Presentacion
 {
     public partial class ReservaForm : Form
     {
-        private ReservaService reservaService;
         private Miembro currentMiembro;
+        private ReservaService reservaService;
         private IClaseRepository claseRepository;
 
         public ReservaForm(Miembro miembro)
         {
             InitializeComponent();
+            currentMiembro = miembro;
+
+            // Se inicializan los servicios y repositorios necesarios.
+            reservaService = new ReservaService(new ReservaRepository(), new MiembroRepository(), new ClaseRepository());
+            claseRepository = new ClaseRepository();
+
+            // Cargar las clases disponibles en el ComboBox.
+            CargarClasesDisponibles();
+
+            dtpFechaReserva.MinDate = DateTime.Now.AddMinutes(30);
+        }
+
+        /// Carga en el ComboBox (cmbClases) la lista de clases disponibles.
+        private void CargarClasesDisponibles()
+        {
+            try
+            {
+                IEnumerable<Clase> clases = claseRepository.ObtenerTodos();
+                // Convertir a lista para evitar problemas de enlace
+                cmbClases.DataSource = new List<Clase>(clases);
+                cmbClases.DisplayMember = "NombreClase";
+                cmbClases.ValueMember = "ClaseID";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar las clases: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        /// Evento click del botón "Reservar" que intenta registrar la reserva.
+        private void btnReservar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Crear el objeto Reserva con la información ingresada.
+                Reserva nuevaReserva = new Reserva();
+                nuevaReserva.UsuarioID = currentMiembro.UsuarioID;
+
+                // Validar que se haya seleccionado una clase
+                if (cmbClases.SelectedItem is Clase claseSeleccionada)
+                {
+                    nuevaReserva.ClaseID = claseSeleccionada.ClaseID;
+                }
+                else
+                {
+                    MessageBox.Show("Por favor, seleccione una clase válida.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                // Asignar la fecha y hora de la reserva
+                nuevaReserva.FechaReserva = dtpFechaReserva.Value;
+
+                // Registrar la reserva usando el servicio, que realiza validaciones internas.
+                int nuevoId = reservaService.RegistrarReserva(nuevaReserva);
+
+                lblMensaje.Text = "Reserva registrada con éxito. ID: " + nuevoId;
+                lblMensaje.ForeColor = Color.Green;
+            }
+            catch (Exception ex)
+            {
+                lblMensaje.Text = "Error al registrar la reserva: " + ex.Message;
+                lblMensaje.ForeColor = Color.Red;
+            }
         }
         private void ReservaForm_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void btnReservar_Click_1(object sender, EventArgs e)
-        {
-
-        }
     }
 }
