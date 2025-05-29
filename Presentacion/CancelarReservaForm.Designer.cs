3536134cebad1457b2666e371864fb111a40996c
valeria.benitez889@pascualbravo.edu.co <valeria.benitez889@pascaulbravo.edu.co>
2025-05-29 06:32:43 -0500

    Formulario para cancelar reservas, con un DataGridView y botón para ejecutar la operación

---
+++ Presentacion/CancelarReservaForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace GymApp.Presentacion
+{
+    partial class CancelarReservaForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvReservas = new System.Windows.Forms.DataGridView();
+            this.btnCancelarReserva = new System.Windows.Forms.Button();
+            this.lblMensaje = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReservas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvReservas
+            // 
+            this.dgvReservas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvReservas.Location = new System.Drawing.Point(179, 93);
+            this.dgvReservas.Name = "dgvReservas";
+            this.dgvReservas.Size = new System.Drawing.Size(240, 150);
+            this.dgvReservas.TabIndex = 0;
+            // 
+            // btnCancelarReserva
+            // 
+            this.btnCancelarReserva.Location = new System.Drawing.Point(488, 305);
+            this.btnCancelarReserva.Name = "btnCancelarReserva";
+            this.btnCancelarReserva.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelarReserva.TabIndex = 1;
+            this.btnCancelarReserva.Text = "button1";
+            this.btnCancelarReserva.UseVisualStyleBackColor = true;
+            // 
+            // lblMensaje
+            // 
+            this.lblMensaje.AutoSize = true;
+            this.lblMensaje.Location = new System.Drawing.Point(502, 148);
+            this.lblMensaje.Name = "lblMensaje";
+            this.lblMensaje.Size = new System.Drawing.Size(35, 13);
+            this.lblMensaje.TabIndex = 2;
+            this.lblMensaje.Text = "label1";
+            // 
+            // CancelarReservaForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblMensaje);
+            this.Controls.Add(this.btnCancelarReserva);
+            this.Controls.Add(this.dgvReservas);
+            this.Name = "CancelarReservaForm";
+            this.Text = "CancelarReservaForm";
+            this.Load += new System.EventHandler(this.CancelarReservaForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReservas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvReservas;
+        private System.Windows.Forms.Button btnCancelarReserva;
+        private System.Windows.Forms.Label lblMensaje;
+    }
+}
