78b6b57b04db4aa1e357963196edcad529846a60
valeria.benitez889@pascualbravo.edu.co <valeria.benitez889@pascaulbravo.edu.co>
2025-05-30 00:03:53 -0500

    Se creó el formulario AccesoForm con controles para ingresar el ID del usuario, el ID de la clase y el estado de acceso

---
+++ Presentacion/AccesoForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace GymApp.Presentacion
+{
+    partial class AccesoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUsuarioID = new System.Windows.Forms.TextBox();
+            this.txtClaseID = new System.Windows.Forms.TextBox();
+            this.cmbEstadoAcceso = new System.Windows.Forms.ComboBox();
+            this.btnRegistrarAcceso = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtUsuarioID
+            // 
+            this.txtUsuarioID.Location = new System.Drawing.Point(295, 71);
+            this.txtUsuarioID.Name = "txtUsuarioID";
+            this.txtUsuarioID.Size = new System.Drawing.Size(100, 20);
+            this.txtUsuarioID.TabIndex = 0;
+            // 
+            // txtClaseID
+            // 
+            this.txtClaseID.Location = new System.Drawing.Point(295, 122);
+            this.txtClaseID.Name = "txtClaseID";
+            this.txtClaseID.Size = new System.Drawing.Size(100, 20);
+            this.txtClaseID.TabIndex = 1;
+            // 
+            // cmbEstadoAcceso
+            // 
+            this.cmbEstadoAcceso.FormattingEnabled = true;
+            this.cmbEstadoAcceso.Location = new System.Drawing.Point(295, 177);
+            this.cmbEstadoAcceso.Name = "cmbEstadoAcceso";
+            this.cmbEstadoAcceso.Size = new System.Drawing.Size(121, 21);
+            this.cmbEstadoAcceso.TabIndex = 2;
+            // 
+            // btnRegistrarAcceso
+            // 
+            this.btnRegistrarAcceso.Location = new System.Drawing.Point(309, 331);
+            this.btnRegistrarAcceso.Name = "btnRegistrarAcceso";
+            this.btnRegistrarAcceso.Size = new System.Drawing.Size(75, 23);
+            this.btnRegistrarAcceso.TabIndex = 7;
+            this.btnRegistrarAcceso.Text = "Acceso";
+            this.btnRegistrarAcceso.UseVisualStyleBackColor = true;
+            // 
+            // AccesoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRegistrarAcceso);
+            this.Controls.Add(this.cmbEstadoAcceso);
+            this.Controls.Add(this.txtClaseID);
+            this.Controls.Add(this.txtUsuarioID);
+            this.Name = "AccesoForm";
+            this.Text = "AccesoForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUsuarioID;
+        private System.Windows.Forms.TextBox txtClaseID;
+        private System.Windows.Forms.ComboBox cmbEstadoAcceso;
+        private System.Windows.Forms.Button btnRegistrarAcceso;
+    }
+}
---
+++ Presentacion/AccesoForm.cs
@@ -0,0 +1,65 @@
+﻿using GymApp.Entidades;
+using GymApp.Negocio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GymApp.AccesoDatos;
+
+namespace GymApp.Presentacion
+{
+    public partial class AccesoForm : Form
+    {
+        private AccesoService accesoService;
+
+        public AccesoForm()
+        {
+            InitializeComponent();
+            // Instanciamos el repositorio y el servicio.
+            IAccesoRepository accesoRepo = new AccesoRepository();
+            accesoService = new AccesoService(accesoRepo);
+        }
+
+        // Evento para registrar un acceso.
+        private void btnRegistrarAcceso_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Validamos que el usuario ingrese un ID válido.
+                if (!int.TryParse(txtUsuarioID.Text, out int usuarioId))
+                {
+                    MessageBox.Show("Por favor, ingrese un ID válido.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+                // Se puede obtener el ID de la clase desde un TextBox o ComboBox.
+                if (!int.TryParse(txtClaseID.Text, out int claseId))
+                {
+                    MessageBox.Show("Por favor, ingrese un ID de clase válido.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+                string estado = cmbEstadoAcceso.Text; // Por ejemplo, "Permitido" o "Denegado"
+
+                // Creamos el objeto acceso.
+                Acceso nuevoAcceso = new Acceso
+                {
+                    UsuarioID = usuarioId,
+                    ClaseID = claseId,
+                    EstadoAcceso = estado,
+                    FechaAcceso = DateTime.Now
+                };
+
+                int idAcceso = accesoService.RegistrarAcceso(nuevoAcceso);
+                MessageBox.Show("Acceso registrado con éxito. ID: " + idAcceso, "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error registrando acceso: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
