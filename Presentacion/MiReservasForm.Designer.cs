a5e8f1a20055f0545d1a3a94edfec413291b1dd5
valeria.benitez889@pascualbravo.edu.co <valeria.benitez889@pascaulbravo.edu.co>
2025-05-31 19:53:03 -0500

    Agregar formulario MiReservasForm para mostrar todas las reservas del miembro autenticado

---
+++ Presentacion/MiReservasForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace GymApp.Presentacion
+{
+    partial class MiReservasForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvMisReservas = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMisReservas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvMisReservas
+            // 
+            this.dgvMisReservas.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvMisReservas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMisReservas.Location = new System.Drawing.Point(90, 97);
+            this.dgvMisReservas.Name = "dgvMisReservas";
+            this.dgvMisReservas.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvMisReservas.Size = new System.Drawing.Size(240, 150);
+            this.dgvMisReservas.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(168, 336);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // MiReservasForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgvMisReservas);
+            this.Name = "MiReservasForm";
+            this.Text = "MiReservasForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMisReservas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvMisReservas;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Presentacion/MiReservasForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using GymApp.AccesoDatos;
+using GymApp.Entidades;
+using GymApp.Negocio;
+
+namespace GymApp.Presentacion
+{
+    public partial class MiReservasForm : Form
+    {
+        private Miembro usuarioActual;
+        private ReservaService reservaService;
+
+        public MiReservasForm(Miembro miembro)
+        {
+            InitializeComponent();
+            usuarioActual = miembro;
+
+            reservaService = new ReservaService(
+                new ReservaRepository(),
+                new MiembroRepository(),
+                new ClaseRepository());
+
+            CargarReservasDelUsuario();
+        }
+
+        private void CargarReservasDelUsuario()
+        {
+            try
+            {
+                var reservas = reservaService.ObtenerReservasPorMiembro(usuarioActual.UsuarioID).ToList();
+                dgvMisReservas.DataSource = reservas;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar tus reservas: " + ex.Message,
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
