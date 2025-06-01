35294f3571c1282ee81cc9eecb48e187519a4f81
valeria.benitez889@pascualbravo.edu.co <valeria.benitez889@pascaulbravo.edu.co>
2025-05-31 19:46:51 -0500

    crear formulario VerClasesForm para visualizar clases disponibles

---
+++ Presentacion/VerClasesForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace GymApp.Presentacion
+{
+    partial class VerClasesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvClases = new System.Windows.Forms.DataGridView();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClases)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvClases
+            // 
+            this.dgvClases.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dgvClases.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvClases.Location = new System.Drawing.Point(71, 70);
+            this.dgvClases.Name = "dgvClases";
+            this.dgvClases.ReadOnly = true;
+            this.dgvClases.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvClases.Size = new System.Drawing.Size(240, 150);
+            this.dgvClases.TabIndex = 0;
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(370, 275);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCerrar.TabIndex = 1;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click_1);
+            // 
+            // VerClasesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.dgvClases);
+            this.Name = "VerClasesForm";
+            this.Text = "VerClasesForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClases)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvClases;
+        private System.Windows.Forms.Button btnCerrar;
+    }
+}
---
+++ Presentacion/VerClasesForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GymApp.AccesoDatos;
+using GymApp.Entidades;
+
+namespace GymApp.Presentacion
+{
+    public partial class VerClasesForm : Form
+    {
+        private ClaseRepository claseRepo;
+
+        public VerClasesForm()
+        {
+            InitializeComponent();
+            claseRepo = new ClaseRepository();
+            CargarClases();
+        }
+
+        private void CargarClases()
+        {
+            try
+            {
+                var clases = claseRepo.ObtenerTodos().ToList();
+                dgvClases.DataSource = clases;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar las clases: " + ex.Message, "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnCerrar_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
